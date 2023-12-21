414c4df720aa2a21c602de2e50fec2377cfd4001
lamusmaser <1940060+lamusmaser@users.noreply.github.com>
2023-12-21 14:16:19 -0700

    Removing attribute checker.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -307,7 +307,7 @@ def PullTASubtitles(vid_metadata, filepath, media_obj):
     for part in item.parts:
       for language in lang_pub_map.keys():
         Log.Debug("Validating keys for {}.".format(lang_pub_map[language]))
-        Log.Debug("Output part details: {}".format(dir(part.subtitles)))
+        Log.Debug("Output part details: {}".format(part.subtitles))
         part.subtitles[language].validate_keys(lang_pub_map[language])
       for language in list(set(part.subtitles.keys()) - set(lang_pub_map.keys())):
         Log.Info("Removing language code '{}' that is no longer available as a locally downloaded subtitle for video ID {}.".format(language, vid_metadata['ytid']))
